5636bcbdf575cf8d33c5f8f5efd0267d64d57ef4
Henry Onu <onuhenry24@gmail.com>
2022-02-04 16:49:31 +0100

    Add spacing to form

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,6 @@
 class Course < ApplicationRecord
     validates :title, presence: true
-    validates :description, length: {minimum: 5}
+    validates :description,presence: true, length: {minimum: 5}
     
     
 end
