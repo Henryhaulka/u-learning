0ccf766022762d5efaef8556bdef181ef0a9af4c
Henry Onu <onuhenry24@gmail.com>
2022-02-15 12:02:16 +0100

    Add user to courses

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,9 @@
+User.create!(email: 'onuhenry24@gmail.com', password: '123456')
 30.times do 
     Course.create!([{ 
      title: Faker::Educator.course_name,
-     description:  Faker::TvShows::GameOfThrones.quote
+     description:  Faker::TvShows::GameOfThrones.quote,
+     user_id: 1
     }])
 end
 
