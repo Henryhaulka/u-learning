42383f3341f3d525841c38949b6c9f451713fa2c
Henry Onu <onuhenry24@gmail.com>
2022-02-21 22:42:26 +0100

    Add Before action for ransack

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
     before_action :authenticate_user!
+    before_action :set_searching_varaible, if: :user_signed_in?
+    
+    def set_searching_varaible
+      @ransack_courses = Course.ransack(params[:courses_search], search_key: :courses_search)  
+    end
+    
 end
