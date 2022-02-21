82592e3253f6d4338121b89ca4b1169022069e28
Henry Onu <onuhenry24@gmail.com>
2022-02-21 10:57:04 +0100

    Add more migrations to the course table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,13 @@
-u = User.find_by!(email: 'onuhenry24@gmail.com')
+# u = User.find_by!(email: 'onuhenry24@gmail.com')
 30.times do 
     Course.create!([{ 
      title: Faker::Educator.course_name,
      description:  Faker::TvShows::GameOfThrones.quote,
-     user_id: u.id
+     user_id: u.id,
+     short_description: Faker::Quote.famous_last_words,
+     level: 'Beginner',
+     language: Faker::ProgrammingLanguage.name,
+     price: Faker::Number.within(range: 10..1000)
     }])
 end
 
