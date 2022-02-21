e121f9ac3f24142aded4f7637f9e5f12686948a3
Henry Onu <onuhenry24@gmail.com>
2022-02-21 10:58:54 +0100

    Add validations and course params of new fields

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,5 @@
 class Course < ApplicationRecord
-    validates :title, presence: true
+    validates :title,:short_description,:price,:level,:language, presence: true
     validates :description,presence: true, length: {minimum: 5}
     has_rich_text :description
     belongs_to :user
