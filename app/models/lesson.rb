bc747710cc0b62c8f1c59cc2c4553565fa17f8bc
Henry Onu <onuhenry24@gmail.com>
2022-03-04 15:38:51 +0100

    Add public activity to lessons

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -2,4 +2,14 @@ class Lesson < ApplicationRecord
   belongs_to :course
   extend FriendlyId
   friendly_id :title, use: :slugged
+
+  include PublicActivity::Model
+  tracked owner: Proc.new{ |controller, model| controller.current_user }
+  
+  #converting the public record to title string
+  def to_s
+    title
+  end
+  
+
 end
