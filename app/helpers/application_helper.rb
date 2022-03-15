25b3a2344a7e1a6920415fdbb5cf7731791df802
Henry Onu <onuhenry24@gmail.com>
2022-03-15 15:12:09 +0100

    Add custom application helpers to view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,22 @@
 module ApplicationHelper
    include Pagy::Frontend
+  def creative_icons(key)
+    case key
+        when 'create'
+           "<i class='fa fa-plus text-success'></i>".html_safe
+        when 'update'
+         "<i class='fa fa-pen text-primary'></i>".html_safe
+        when 'destroy'
+         "<i class='fa fa-trash text-danger'></i>".html_safe     
+    end
+  end
+  
+  def model_type(model)
+    case model
+        when 'Course'
+           "<i class='fa fa-graduation-cap '></i>".html_safe
+        when 'Lesson'
+         "<i class='fa fa-tasks '></i>".html_safe    
+    end
+  end 
 end
