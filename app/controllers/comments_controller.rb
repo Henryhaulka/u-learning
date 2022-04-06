945f5620198115648f8ffe7de289da25586c2ca1
Henry Onu <onuhenry24@gmail.com>
2022-04-06 11:59:03 +0100

    Add logic for creating comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,25 @@
 class CommentsController < ApplicationController
+    def new
+        
+    end
+    
+    
+    def create
+       @course = Course.friendly.find(params[:course_id])
+       @lesson = Lesson.friendly.find(params[:lesson_id])
+       @comment = Comment.new(set_comment)
+       @comment.lesson =  @lesson
+       @comment.user = current_user
+      if @comment.save
+         redirect_to course_lesson_url(@course,@lesson), notice: "Comment was successfully created." 
+      else
+          render 'lessons/comments/new'
+      end
+    end
+
+    private 
+    def set_comment
+        params.require(:comment).permit(:content)
+    end
+    
 end
