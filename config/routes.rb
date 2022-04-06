22f5728b7553ec7c7140ec85faab84a0aedf848f
Henry Onu <onuhenry24@gmail.com>
2022-04-06 12:01:22 +0100

    Add views for creating comments

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
       patch :unapprove
     end
     resources :lessons, except: :index do
-      resources :comments
+      resources :comments, except: :index
       put :sort
       member do
         delete :delete_video
