f857962bd8ce35554f4c9d5c8bd58eb897ff90d3
Henry Onu <onuhenry24@gmail.com>
2022-04-06 16:50:58 +0100

    Add counter_cache to user and lesson for comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,7 @@
 class Comment < ApplicationRecord
-  belongs_to :user
-  belongs_to :lesson
+  belongs_to :user, counter_cache: true
+    # User.find_each {|user|User.reset_counters(user.id, :comments )}
+  belongs_to :lesson, counter_cache: true
+  # Lesson.find_each {|lesson|Lesson.reset_counters(lesson.id, :comments )}
   validates :content, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_06_084018) do
+ActiveRecord::Schema.define(version: 2022_04_06_153659) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -122,6 +122,7 @@ ActiveRecord::Schema.define(version: 2022_04_06_084018) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "slug"
     t.integer "row_order"
+    t.integer "comments_count", default: 0, null: false
     t.index ["course_id"], name: "index_lessons_on_course_id"
     t.index ["slug"], name: "index_lessons_on_slug", unique: true
   end
@@ -180,6 +181,7 @@ ActiveRecord::Schema.define(version: 2022_04_06_084018) do
     t.string "slug"
     t.integer "subscriptions_count", default: 0, null: false
     t.integer "courses_count", default: 0, null: false
+    t.integer "comments_count", default: 0, null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
