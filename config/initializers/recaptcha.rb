4ea9216c1ad168801b7b06ca72d400048c2deda7
Henry Onu <onuhenry24@gmail.com>
2022-04-07 16:48:11 +0100

    hide credentials

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,6 @@
 Recaptcha.configure do |config|
-  config.site_key  = '6LdAqVMfAAAAACrbnplGvVm4cbG4YidUdaIL5BL3'
-  config.secret_key = '6LdAqVMfAAAAAFNlEMFCeK9BoQpL8yr06ZrspTI_'
+  config.site_key  = Rails.application.credentials.recaptcha[:site_key]
+  config.secret_key = Rails.application.credentials.recaptcha[:secret_key]
 
   # Uncomment the following line if you are using a proxy server:
   # config.proxy = 'http://myproxy.com.au:8080'
