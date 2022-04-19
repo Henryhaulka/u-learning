9ee2d91b50a0880cef512d7350775ce84f4eb76b
Henry Onu <onuhenry24@gmail.com>
2022-04-19 19:36:53 +0100

    Add update and update show action

--- app/controllers/courses/course_wizard_controller.rb
+++ app/controllers/courses/course_wizard_controller.rb
@@ -1,22 +1,34 @@
 class Courses::CourseWizardController < ApplicationController
     include Wicked::Wizard
     # before action should not be b4 the include module
-  before_action :set_progress, only: :show
+  before_action :set_course, only: [:show, :finish_wizard_path, :update]
+  before_action :set_progress, only: [:show, :update]
   steps :basic_info, :details
 
     def show
-        # @user = current_user
-        # case step
-        # when :find_friends
-        #     @friends = @user.find_friends
-        # end
+       case step
+            when :basic_info
+            when :details
+              @tags = Tag.all
+        end
         render_wizard
     end
 
     def finish_wizard_path
-        @course = Course.friendly.find(params[:course_id])
         course_path(@course)
     end
+
+    def update
+        case step
+            when :basic_info
+              @course.update(course_params)
+            when :details
+              @tag = Tag.all
+              @course.update(course_params)
+        end
+         render_wizard @course #render wizard 4 course
+    end
+    
      
     private
     def set_progress
@@ -26,5 +38,14 @@ class Courses::CourseWizardController < ApplicationController
             @progress = 0
         end
     end
+
+    def course_params
+      params.require(:course).permit(:title, :description, :short_description, :level, :published, :price, :language, :avatar, tag_ids: [])
+    end
+
+    def set_course
+      @course = Course.friendly.find(params[:course_id])
+    end
+
     
 end
